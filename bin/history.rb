fa58bbdf8e2c3d4bd99d694bbbf27107f30fad57
Victor Mours <victor.mours@gmail.com>
2016-11-13 19:22:26 +0100

    Working display of unused and little used aliases

--- bin/history.rb
+++ bin/history.rb
@@ -1,35 +1,67 @@
 #!/usr/bin/env ruby
 
-logs = File.readlines("#{ENV['HOME']}/.zsh_history")
-latest_logs = logs[-10..-1]
+class Command
+  def self.all
+    logs = File.readlines("#{ENV['HOME']}/.zsh_history")
 
-log_commands = logs.map do |log_line|
-  log_line.force_encoding("iso-8859-1").split(";").last.chomp.split(" ").first
+    logs.map do |log_line|
+      log_line.force_encoding("iso-8859-1").split(";").last.chomp.split(" ").first
+    end
+  end
+
+  def self.counts
+    all.group_by { |command| command }.map do |command, commands|
+      [command, commands.count]
+    end
+  end
 end
 
-aliases = `echo "source #{ENV['HOME']}/.zshrc; alias -L" | /bin/zsh`.split("\n").select do |alias_line|
-  alias_line[/alias\s+[^=]*=/]
+class Alias < Struct.new(:string)
+
+  def self.all
+    @all ||= `echo "source #{ENV['HOME']}/.zshrc; alias -L" | /bin/zsh`.split("\n").select do |alias_line|
+      alias_line[/alias\s+[^=]*=/]
+    end.map do |string|
+      new(string)
+    end
+  end
+
+  def self.find_by_command(command)
+    all.find do |aliaz|
+      aliaz.command == command
+    end
+  end
+
+  def abbreviation
+    string.match(/alias\s+([^=]*)=/)[1]
+  end
+
+  def command
+    string.match(/='?"?([^'"]*)'?"?/)[1]
+  end
+
 end
 
-alias_commands = aliases.map do |alias_line|
-  alias_line.match(/alias\s+([^=]*)=/)[1]
+aliases = Alias.all
+
+alias_commands = aliases.map(&:command)
+
+commands_by_counts = Command.counts.to_h
+
+unused_aliases = Alias.all.select do |aliaz|
+  commands_by_counts[aliaz.abbreviation] == nil
 end
 
-grouped_commands = log_commands.group_by do |command|
-  command
+puts "You have never used these aliases in the past #{Command.all.count} commands:"
+puts unused_aliases.map(&:string)
+
+puts "You should remove them from your zshrc."
+
+used_once_aliases = Alias.all.select do |aliaz|
+  commands_by_counts[aliaz.abbreviation] == 1
 end
 
-commands_by_counts = grouped_commands.map do |command, commands|
-  [command, commands.count]
-end.sort_by do |command_count|
-  -command_count[1]
-end.to_h
-
-unused_aliases = alias_commands - commands_by_counts.map(&:first)
-puts "You are not using these aliases:"
-puts unused_aliases
-
-alias_usage = alias_commands.map do |alias_command|
-  [alias_command, commands_by_counts[alias_command]]
-end
-
+3.times { puts '' }
+puts "You have used these aliases only once in the past #{Command.all.count} commands:"
+puts used_once_aliases.map(&:string)
+puts "You should consider removing them from your zshrc."
